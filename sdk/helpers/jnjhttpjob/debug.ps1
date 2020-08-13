eb3c6674277dd7350d7ed0d55637efcf5cdab263
Matthew Haugen <mhaugen1@its.jnj.com>
2020-08-13 13:45:30 -0700

    Move files around

---
+++ sdk/helpers/jnjhttpjob/debug.ps1
@@ -0,0 +1,10 @@
+
+Push-Location $PSScriptRoot\..\..;
+Try {
+    pip install .\jnjjobwrapper
+    $env:JOB_CONFIG_PATH="./sample_job/config.json"
+    uvicorn main:app
+}
+Finally {
+    Pop-Location
+}
