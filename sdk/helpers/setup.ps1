a35c652b6b6ac4ea092832cc45b05ff472f41975
Matthew Haugen <mhaugen1@its.jnj.com>
2020-08-13 13:47:53 -0700

    more file moves

---
+++ sdk/helpers/setup.ps1
@@ -0,0 +1,2 @@
+pip install -e $PSScriptRoot\..\jnjjobwrapper\
+pip install -e $PSScriptRoot\..\jnjhttpjob\
--- sdk/setup.ps1
+++
@@ -1,2 +0,0 @@
-pip install -e $PSScriptRoot\.\jnjjobwrapper\
-pip install -e $PSScriptRoot\.\jnjhttpjob\
