b0ae4d30ca90ccdb2477310c9a0be0b24b382e5e
Matthew Haugen <mhaugen@haugenapplications.com>
2020-08-17 15:31:20 -0700

    add pyproject.toml

--- src/mlservicewrapper/setup.py
+++ src/mlservicewrapper/setup.py
@@ -2,7 +2,10 @@ from setuptools import find_packages, setup
 
 setup(
    name='mlservicewrapper',
-   use_scm_version=True,
+   use_scm_version={
+      "root": "..",
+      "relative_to": __file__
+   },
    description='Configure a Python service for repeated execution',
    author='Matthew Haugen',
    author_email='mhaugen@haugenapplications.com',
