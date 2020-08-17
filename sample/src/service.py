13f20e06253c57aa8254bee7962fbb57a692ef83
Matthew Haugen <mhaugen@haugenapplications.com>
2020-08-17 16:59:32 -0700

    Use PascalCase for datasets

--- sample/src/service.py
+++ sample/src/service.py
@@ -16,11 +16,11 @@ class SampleService(mlservicewrapper.services.Service):
         self.__mod_by = int(ctx.get_parameter_value("ModBy", default="2"))
 
     async def process(self, ctx: mlservicewrapper.contexts.ProcessContext):
-        input_data = await ctx.get_input_dataframe("data")
+        input_data = await ctx.get_input_dataframe("Data")
 
         if "TextField" not in input_data.columns:
-            raise mlservicewrapper.errors.MissingDatasetFieldError("data", "TextField")
+            raise mlservicewrapper.errors.MissingDatasetFieldError("Data", "TextField")
 
         result = dal.process(input_data, self.__mod_by)
 
-        await ctx.set_output_dataframe("results", result)
+        await ctx.set_output_dataframe("Results", result)
