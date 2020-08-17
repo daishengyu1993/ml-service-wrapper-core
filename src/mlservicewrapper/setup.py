4adcc8121c6954bd63e0f4fd6ccd31b4dc660350
Matthew Haugen <mhaugen@haugenapplications.com>
2020-08-17 14:51:10 -0700

    use scm for setup version

--- src/mlservicewrapper/setup.py
+++ src/mlservicewrapper/setup.py
@@ -2,12 +2,13 @@ from setuptools import find_packages, setup
 
 setup(
    name='mlservicewrapper',
-   version='0.2',
+   use_scm_version=True,
    description='Configure a Python service for repeated execution',
    author='Matthew Haugen',
    author_email='mhaugen@haugenapplications.com',
    packages=find_packages(),
    install_requires=[
       "pandas"
-   ]
+   ],
+   setup_requires=['setuptools_scm']
 )
