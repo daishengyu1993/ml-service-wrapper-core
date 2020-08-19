9f298730b246cef426299dc8641f7b44cbe494a4
Matthew Haugen <mhaugen@haugenapplications.com>
2020-08-19 09:06:58 -0700

    Release pipeline; migrate out http host

--- src/setup.py
+++ src/setup.py
@@ -1,5 +1,5 @@
 import os
-from setuptools import find_namespace_packages, setup
+from setuptools import find_namespace_packages, setup, find_packages
 
 if os.environ.get("USE_SCM_VERSION"):
    use_scm_version = {
@@ -12,16 +12,18 @@ else:
    version = "0.0.0"
 
 setup(
-   name='mlservicewrapper-http',
+   name='mlservicewrapper-core',
    version = version,
    use_scm_version = use_scm_version,
    description='Configure a Python service for repeated execution',
    author='Matthew Haugen',
    author_email='mhaugen@haugenapplications.com',
+   #packages=find_packages(),
    packages=find_namespace_packages(include=['mlservicewrapper.*']),
    install_requires=[
-      "mlservicewrapper-core",
-      "starlette==0.13.7"
+      "pandas"
    ],
-   setup_requires=['setuptools_scm']
+   #namespace_packages=['mlservicewrapper.core.errors'],
+   setup_requires=['setuptools_scm'],
+   zip_safe=False
 )
